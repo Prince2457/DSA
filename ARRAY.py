f839d73cfca28512fdf75dc1d5787ce4db7e5d71
Prince Nkansah <nkansahp466@gmail.com>
2026-05-10 15:55:59 +0000

    week 13 : three sum - full understanding

--- ARRAY.py
+++ ARRAY.py
@@ -21,7 +21,8 @@ def two_sum(nums, target):
         need = target - nums[i]
         if need in seen: 
             return [seen[need], i]
-        seen[nums[i]] = i  
+        seen[nums[i]] = i 
+        
 
 def find_duplicates(data):
     for i in range (len(data)):
@@ -41,13 +42,14 @@ def find_duplicates(data):
 
 
 def isAnagram( s, t):
+
     if len(s) != len(t):
         return False
     stored = {}
     for i in s:
         letter_count =s.count(i)
         stored[i] = letter_count 
-    for x in s:
+    for x in t:
         if t not in stored:
             return False
         if stored[t] != t.count(x):
@@ -71,7 +73,7 @@ def is_Anagram(s,t):
     if stored != stored2:
         return False
     return True
-print(is_Anagram(s,t))          
+
 
 def twoSum(numbers, target):
     left = 0
@@ -86,5 +88,45 @@ def twoSum(numbers, target):
             right -= 1   
 
 
+def isPalindrome(s= str) -> bool:
+    cleaned = ""
+    for char in s:
+        if char.isalnum():
+            cleaned += char.lower()
+    left = 0
+    right = len(cleaned)-1
+    while left < right:
+        if cleaned[left] == cleaned[right]:
+            left += 1
+            right -= 1
+        else:
+            return False
+    return True    
+
+print(two_sum([1,2,3,4], 4))
+
+def three_sum(nums):
+    nums.sort()
+    result = []
+    for i in  range(len(nums)):
+        target = -nums[i]
+        left = i + 1
+        right = len(nums)- 1
+        if i > 0 and nums[i] == nums[i-1]:
+            continuegit
+        while left < right:
+            if nums[left] + nums[right] < target:
+                left += 1
+            elif nums[left] + nums[right] > target:
+                right -= 1
+            else:
+                result.append([nums[i],nums[left], nums[right]])
+                left +=1
+                right -= 1
+                while nums[left] < nums[right] and nums[left] == nums[left -1]:
+                    nums[left] += 1
+    return result    
 
 
+
+print(three_sum([-1, 0, 1, 2, -1, -1]))
